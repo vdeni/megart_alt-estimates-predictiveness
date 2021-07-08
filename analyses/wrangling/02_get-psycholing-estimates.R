23b994680378e1baa58b62eed23a88d01f696107
Denis Vlašiček <denis.vlasicek@gmail.com>
2021-07-08 16:36:47 +0200

    add median & mean for subfreq

--- analyses/wrangling/02_get-psycholing-estimates.R
+++ analyses/wrangling/02_get-psycholing-estimates.R
@@ -90,3 +90,84 @@ rm(d_image_dupes,
    v_image_dupes)
 
 ##### subjective frequency #####
+d_subfreq <- readr::read_tsv(here::here('data',
+                                        'psycholinguistic-estimates',
+                                        'subjective-frequency.tsv'),
+                           col_names = F,
+                           col_types = paste0('c',
+                                              paste0(rep('i', 36),
+                                                     collapse = '')))
+
+d_subfreq %<>%
+    magrittr::set_colnames(.,
+                           c('string',
+                           paste0('rater_', 1:(ncol(.) - 1))))
+
+# remove duplicated entries
+d_subfreq %<>%
+    dplyr::distinct(.)
+
+# some words appeared in both data-collection waves. i'm separating those words
+# out into their own dataframe, to get a estimate
+v_subfreq_dupes <- d_subfreq[duplicated(d_subfreq$string), 'string'] %>%
+    dplyr::pull(.,
+                string)
+
+d_subfreq_dupes <- d_subfreq %>%
+    dplyr::filter(.,
+                  string %in% v_subfreq_dupes)
+
+d_subfreq %<>%
+    dplyr::filter(.,
+                  !string %in% v_subfreq_dupes)
+
+# get mean and median for non-duplicates
+d_subfreq$estimate_mean <- d_subfreq %>%
+    dplyr::select(.,
+                  matches('^rater')) %>%
+    rowMeans(.,
+             na.rm = T)
+
+d_subfreq$estimate_median <- d_subfreq %>%
+    dplyr::select(.,
+                  matches('^rater')) %>%
+    apply(.,
+          MARGIN = 1,
+          FUN = median,
+          na.rm = T)
+
+# get mean and median for duplicates
+d_subfreq_dupes %<>%
+    dplyr::group_by(.,
+                    string) %>%
+    tidyr::nest(.)
+
+d_subfreq_dupes$data <- d_subfreq_dupes$data %>%
+    purrr::map(.,
+               .f = ~as.matrix(as.data.frame(.x)))
+
+d_subfreq_dupes$estimate_mean <- d_subfreq_dupes$data %>%
+    purrr::map_dbl(.,
+                   .f = mean,
+                   na.rm = T)
+
+d_subfreq_dupes$estimate_median <- d_subfreq_dupes$data %>%
+    purrr::map_dbl(.,
+                   .f = median,
+                   na.rm = T)
+
+d_subfreq_dupes %<>%
+    dplyr::select(.,
+                  -data) %>%
+    dplyr::ungroup(.)
+
+# combine estimates
+d_subfreq %<>%
+    dplyr::select(.,
+                  -matches('rater'))
+
+d_subfreq <- bind_rows(d_subfreq,
+                       d_subfreq_dupes)
+
+rm(d_subfreq_dupes,
+   v_subfreq_dupes)
