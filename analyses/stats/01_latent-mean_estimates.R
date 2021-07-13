ec379ed597664bccc03e025d52cc14001a765094
Denis Vlašiček <denis.vlasicek@gmail.com>
2021-07-13 17:05:27 +0200

    seemingly half-functional model for all strings

--- analyses/stats/01_latent-mean_estimates.R
+++ analyses/stats/01_latent-mean_estimates.R
@@ -5,20 +5,24 @@ library(cmdstanr)
 source(here::here('wrangling',
                   '02_prepare-psycholing-data.R'))
 
-d <- slice(d_image, 1) %>%
+d <- filter(d_image, string %in% c('razmisliti', 'sezati')) %>%
     select(-c(mean, median)) %>%
+    mutate(string_id = 1:nrow(.)) %>%
     pivot_longer(cols = matches('rater'),
                  names_to = 'rater',
-                 values_to = 'rating')
+                 values_to = 'rating') %>%
+    drop_na()
 
 m_probit <- cmdstanr::cmdstan_model(here::here('stats',
                                                '02_latent-mean_model.stan'))
 
 m_probit_out <- m_probit$sample(data = list('K' = 5,
-                                            'N' = length(d$rating[!is.na(d$rating)]),
-                                            'Y' = d$rating[!is.na(d$rating)],
+                                            'N' = length(d$rating),
+                                            'S' = max(d$string_id),
+                                            'string_id' = d$string_id,
                                             'c_1' = 1.5,
-                                            'c_4' = 4.5),
+                                            'c_4' = 4.5,
+                                            'Y' = d$rating),
                                 chains = 6,
                                 iter_warmup = 3e3,
                                 iter_sampling = 5e3)
