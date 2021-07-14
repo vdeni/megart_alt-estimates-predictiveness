5fde85f2134ad51e8d24e159c1301089ad064a22
Denis Vlašiček <denis.vlasicek@gmail.com>
2021-07-14 17:04:44 +0200

    preparing loop for latent estimates

--- analyses/stats/01_latent-mean_estimates.R
+++ analyses/stats/01_latent-mean_estimates.R
@@ -1,24 +1,77 @@
 library(here)
 library(cmdstanr)
+library(tidyr)
 
 # load dataframes with estimates
 source(here::here('wrangling',
                   '02_prepare-psycholing-data.R'))
 
-d <- slice(d_image, 1) %>%
-    select(-c(mean, median)) %>%
-    pivot_longer(cols = matches('rater'),
-                 names_to = 'rater',
-                 values_to = 'rating')
-
+# compile model
 m_probit <- cmdstanr::cmdstan_model(here::here('stats',
                                                '02_latent-mean_model.stan'))
 
+##### imageability #####
+# add string id
+d_image %<>%
+    dplyr::mutate(.,
+                  string_id = 1:nrow(.))
+
+# transform data to long for analysis
+d_image_long <- d_image %>%
+    tidyr::pivot_longer(.,
+                        cols = matches('rater'),
+                        names_to = 'rater',
+                        values_to = 'rating') %>%
+    tidyr::drop_na(.)
+
+# loop over words, extract estimates
+# initialize tibble for storing estimates
+estimates <- tibble()
+
+for (i in 1:nrow(d_image[1:3, ])) {
+    .data <- dplyr::filter(d_image_long,
+                           string_id == i)
+
+    .m_probit_samples <- m_probit$sample(data = list('K' = 5,
+                                                     'N' = length(.data$rating),
+                                                     'Y' = .data$rating,
+                                                     'c_1' = 1.5,
+                                                     'c_4' = 4.5),
+                                         chains = 6,
+                                         iter_warmup = 3e3,
+                                         iter_sampling = 6e3)
+
+    .out <- .m_probit_samples$summary() %>%
+        dplyr::select(.,
+                      variable,
+                      mean,
+                      sd,
+                      q5,
+                      q95,
+                      rhat,
+                      ess_bulk,
+                      ess_tail) %>%
+        dplyr::filter(.,
+                      variable %in% c('mu',
+                                      'sigma')) %>%
+        tidyr::pivot_wider(.,
+                           names_from = 'variable',
+                           names_glue = '{variable}_{.value}',
+                           values_from = mean:ess_tail)
+
+    .out %<>%
+        dplyr::mutate(.,
+                      'string_id' = i)
+
+    estimates <- bind_rows(estimates,
+                           .out)
+}
+
 m_probit_out <- m_probit$sample(data = list('K' = 5,
-                                            'N' = length(d$rating[!is.na(d$rating)]),
-                                            'Y' = d$rating[!is.na(d$rating)],
+                                            'N' = length(.data$rating),
+                                            'Y' = .data$rating,
                                             'c_1' = 1.5,
                                             'c_4' = 4.5),
                                 chains = 6,
                                 iter_warmup = 3e3,
-                                iter_sampling = 5e3)
+                                iter_sampling = 6e3)
