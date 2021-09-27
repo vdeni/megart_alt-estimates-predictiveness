9e6cb8841a238af7c4005474a17379be980d47b0
Denis Vlašiček <denis.vlasicek@gmail.com>
2021-09-27 17:15:52 +0200

    fix path in stats/ shell scripts

--- analyses/stats/05_reaction-time_loglik.sh
+++ analyses/stats/05_reaction-time_loglik.sh
@@ -4,4 +4,4 @@ set -eu -o pipefail
 cd ..
 
 printf "\n\n>>>>>>>>>> Running R script.\n\n"
-Rscript 05_reaction-time_loglik.R --args $1
+Rscript stats/05_reaction-time_loglik.R --args $1
--- analyses/stats/07_plot-posterior-check.sh
+++ analyses/stats/07_plot-posterior-check.sh
@@ -4,4 +4,4 @@ set -eu -o pipefail
 cd ..
 
 printf "\n\n>>>>>>>>>> Running R posterior plot script.\n\n"
-Rscript 07_plot-posterior-check.R --args $1
+Rscript stats/07_plot-posterior-check.R --args $1
