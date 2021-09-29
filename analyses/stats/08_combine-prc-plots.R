282163353fc20e04f1c3fe2837e375846d9f0152
Denis Vlašiček <denis.vlasicek@gmail.com>
2021-09-29 18:33:14 +0200

    add png save

--- analyses/stats/08_combine-prc-plots.R
+++ analyses/stats/08_combine-prc-plots.R
@@ -2,6 +2,8 @@ library(here)
 library(ggplot2)
 library(ggpubr)
 
+.plot_width <- 12
+
 v_plots <- list.dirs(here::here('stats')) %>%
     grep(x = .,
          pattern = 'model',
@@ -13,6 +15,20 @@ v_plots <- list.dirs(here::here('stats')) %>%
 l_plots <- lapply(v_plots,
                   readRDS)
 
-ggpubr::ggarrange(plotlist = l_plots,
-                  nrow = 3,
-                  ncol = 1)
+l_plots[[1]] <- l_plots[[1]] +
+    labs(x = '')
+
+l_plots[[2]] <- l_plots[[2]] +
+    labs(x = '')
+
+.p <- ggpubr::ggarrange(plotlist = l_plots,
+                        nrow = 3,
+                        ncol = 1)
+
+ggplot2::ggsave(plot = .p,
+                filename = here::here('stats',
+                                      'p_prc_comparison.png'),
+                device = 'png',
+                bg = '#FFFFFF',
+                width = .plot_width,
+                height = .plot_width * 9 / 16)
