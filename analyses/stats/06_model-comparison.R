494ad6f49b7f6f32fae3f262fc1840888d781dad
Denis Vlašiček <denis.vlasicek@gmail.com>
2021-09-30 11:50:32 +0200

    add model comparison table

--- analyses/stats/06_model-comparison.R
+++ analyses/stats/06_model-comparison.R
@@ -12,3 +12,7 @@ loo_median <- readRDS(here::here('stats',
 loo_latent_mean <- readRDS(here::here('stats',
                                       'model-out-data_latent-mean',
                                       'loo_latent-mean.RData'))
+
+loo::loo_compare(loo_mean,
+                 loo_median,
+                 loo_latent_mean)
