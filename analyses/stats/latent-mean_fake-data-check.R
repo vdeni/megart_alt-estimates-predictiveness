09052817467fcb5d593dbce5479c71d6e378b377
Denis Vlašiček <denis.vlasicek@gmail.com>
2022-01-10 14:24:07 +0100

    add param recovery plot

---
+++ analyses/stats/latent-mean_fake-data-check.R
@@ -0,0 +1,45 @@
+library(cmdstanr)
+library(here)
+library(magrittr)
+library(dplyr)
+library(readr)
+
+d <- readr::read_csv(here::here('stats',
+                                'latent-mean_fake-data.csv'))
+
+m_probit <- cmdstanr::cmdstan_model(here::here('stats',
+                                               'latent-mean_model.stan'))
+
+summaries <- dplyr::tibble()
+
+for (i in 1:max(unique(d$.iter))) {
+    d_subset <- filter(d,
+                       .iter == i)
+
+    datlist <- list('K' = 5,
+                    'N' = length(d_subset$rating),
+                    'Y' = d_subset$rating,
+                    'c_1' = 1.5,
+                    'c_4' = 4.5)
+
+    .m_probit_samples <- m_probit$sample(data = datlist,
+                                         chains = 6,
+                                         parallel_chains = 6,
+                                         iter_warmup = 2e3,
+                                         iter_sampling = 3e3)
+
+    .summary <- .m_probit_samples$summary() %>%
+        dplyr::filter(.,
+                      variable == 'mi') %>%
+        dplyr::select(.,
+                      'mean',
+                      'q5',
+                      'q95')
+
+    summaries <- dplyr::bind_rows(summaries,
+                                  .summary)
+}
+
+readr::write_csv(summaries,
+                 here('stats',
+                      'latent-mean_fake-data_summaries.csv'))
---
+++ analyses/stats/latent-mean_fake-data-plot.R
@@ -0,0 +1,48 @@
+library(here)
+library(readr)
+library(dplyr)
+library(magrittr)
+library(ggplot2)
+
+theme_set(theme_minimal())
+
+d_summaries <- readr::read_csv(here::here('stats',
+                                          'latent-mean_fake-data_summaries.csv'))
+
+d_fake <- read_csv(here('stats',
+                        'latent-mean_fake-data.csv'))
+
+# parameter recovery check
+d_fake_mi <- dplyr::select(d_fake,
+                           mi,
+                           .iter) %>%
+    dplyr::distinct(.)
+
+d_summaries %<>%
+    dplyr::mutate(.,
+                  .iter = 1:nrow(.))
+
+d_plot <- dplyr::left_join(d_summaries,
+                           d_fake_mi,
+                           by = '.iter')
+
+d_plot$in_interval <- ifelse(d_plot$mi >= d_plot$q5 & d_plot$mi <= d_plot$q95,
+                             T,
+                             F)
+
+ggplot2::ggplot(d_plot,
+                aes(x = mi,
+                    y = mean,
+                    group = .iter)) +
+    ggplot2::geom_point(size = 3,
+                        alpha = .7) +
+    ggplot2::geom_errorbar(aes(ymin = q5,
+                               ymax = q95)) +
+    ggplot2::geom_abline(slope = 1) +
+    ggplot2::scale_x_continuous(breaks = 1:5,
+                                labels = 1:5,
+                                minor_breaks = NULL) +
+    ggplot2::scale_y_continuous(breaks = 1:5,
+                                labels = 1:5) +
+    ggplot2::coord_cartesian(xlim = c(1, 5),
+                             ylim = c(1, 5))
